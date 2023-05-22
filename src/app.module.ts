3328310e4b5b80ddd15eae4ed9e9bcfd58127425
enpitsu882 <enpitsu882@hotmail.co.jp>
2023-05-22 21:57:40 +0900

    databaseとの接続まで

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,18 @@ import { ItemsModule } from './items/items.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [ItemsModule, TypeOrmModule.forRoot()],
+  imports: [
+    ItemsModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'postgres',
+      autoLoadEntities: true,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
